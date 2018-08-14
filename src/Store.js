1c7e6a2ce7a6bc4e1c1e21bcdbf435c4fb82e946
Vijay Kumar <38584330+vijay48194@users.noreply.github.com>
2018-08-14 16:18:39 +0530

    Add files via upload

--- src/Store.js
+++ src/Store.js
@@ -1,22 +1,22 @@
 import {decorate,observable,action,computed} from 'mobx';
 
 class Store{
-	productList=[
+	@observable	productList=[
 	{name:'Milk',price:10},
 	{name:'Bread',price:5}
 
 	]
-	addProduct(name,price){
+	@action addProduct(name,price){
 		this.productList.push({
 			name:name,
 			price:price
 		})
 
 	}
-	 get productCount(){
+	@computed get productCount(){
 		return this.productList.length
 	}
-	get totalPrice(){
+	@computed	get totalPrice(){
 		return this.productList.reduce((n,p)=>{
 			return n + p.price;
 		},0)
@@ -24,12 +24,12 @@ class Store{
 
 }
 
-decorate(Store,{
-	productList :observable,
-	addProduct:action,
-	productCount: computed,
-	totalPrice:computed
-})
+// decorate(Store,{
+// 	productList :observable,
+// 	addProduct:action,
+// 	productCount: computed,
+// 	totalPrice:computed
+// })
 const appStore= new Store();
 window.appStore=appStore;
 export default appStore;
